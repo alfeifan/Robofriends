6ad4d2aa16d2dd573f2beb240d3da9d1ef777c8e
alfeifan <46737644+alfeifan@users.noreply.github.com>
2020-05-22 20:20:28 +1000

    Made project production ready and removed debug error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,20 +3,17 @@ import Card from './Card';
 
 
 const CardList = ({ robots }) => {
-	if (true) {
-		throw new Error("NOOOO!");
-	}
 	return (
 		<div>
 		{
 			robots.map((user, i) => {
 				return (
-					<Card 
-						key={i} 
-						id={robots[i].id} 
-						name={robots[i].name} 
-						email={robots[i].email}/>
-						);
+				<Card 
+					key={i} 
+					id={robots[i].id} 
+					name={robots[i].name} 
+					email={robots[i].email}/>
+				);
 			})
 		}
 		</div>
